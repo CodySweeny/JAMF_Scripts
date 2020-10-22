65cd324534c200c220534d0da49aeb2f907c7ca2
CodySweeny <73262426+CodySweeny@users.noreply.github.com>
2020-10-21 19:33:16 -0500

    Add files via upload

---
+++ FireEye.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+#Description: EA to check FireEye EndPoint Security HX Agent Version.
+#
+RESULT="Not Installed"
+if [ -f "/Library/FireEye/xagt/xagt.app/Contents/Info.plist" ] ; then
+    RESULT=$( defaults read /Library/FireEye/xagt/xagt.app/Contents/Info CFBundleVersion )
+fi
+echo "<result>$RESULT</result>"
